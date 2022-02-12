f9604ad19b38c6786986334d08ec2e49036f4c25
Bagel Sandwich <Calvindc96@gmail.com>
2022-02-12 18:28:36 +0100

    Added Constants and InvalidTransactionException

---
+++ app/src/main/java/com/decline/moneymanagerv2/app_features/data/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.decline.moneymanagerv2.app_features.data.util
+
+object Constants {
+
+    const val DATABASE_NAME = "money_manager_db"
+}
---
+++ app/src/main/java/com/decline/moneymanagerv2/app_features/domain/util/InvalidTransactionException.kt
@@ -0,0 +1,3 @@
+package com.decline.moneymanagerv2.app_features.domain.util
+
+class InvalidTransactionException(message: String) : Exception(message)
