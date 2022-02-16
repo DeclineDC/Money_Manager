8d1acfdb0b0715f9ff0ddc2d191e03d82ed7730f
Bagel Sandwich <Calvindc96@gmail.com>
2022-02-16 18:52:02 +0100

    Added TopBar

---
+++ app/src/main/java/com/decline/moneymanagerv2/app_features/presentation/add_edit_transaction_screen/components/TopBar.kt
@@ -0,0 +1,43 @@
+package com.decline.moneymanagerv2.app_features.presentation.add_edit_transaction_screen.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.decline.moneymanagerv2.R
+import com.decline.moneymanagerv2.ui.theme.LocalSpacing
+
+@Composable
+fun TopBar(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val spacing = LocalSpacing.current
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(spacing.spaceSmall)
+    ) {
+        Icon(
+            imageVector = Icons.Default.ArrowBack,
+            contentDescription = stringResource(R.string.back),
+            Modifier
+                .clickable {
+                    onClick
+                }
+        )
+        Spacer(modifier = Modifier.padding(spacing.spaceSmall))
+        Text(text = text, style = MaterialTheme.typography.h3)
+    }
+}
